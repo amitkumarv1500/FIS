856e9c3a86920a429de93af5762a80080c5878ce
Amit Kumar Verma <amitkumarv003@gmail.com>
2025-01-06 21:46:13 +0530

    FIS interview

--- src/test/java/RestAPI_Automation.java
+++ src/test/java/RestAPI_Automation.java
@@ -2,35 +2,15 @@ import common.WebActionLib;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-import pages.ItemListingPage;
-import pages.LandingPage;
-import pages.SearchResultPage;
-import pages.ShoppingCartPage;
-
 import java.util.LinkedHashMap;
-import java.util.concurrent.TimeUnit;
 
 public class RestAPI_Automation {
-
-    WebDriver driver;
-    JavascriptExecutor js;
-    LandingPage landingPage;
-    SearchResultPage searchResultPage;
-    ItemListingPage itemListingPage;
-    ShoppingCartPage shoppingCartPage;
-    WebActionLib lib;
-
-    @BeforeSuite
-    public void setup() {}
 
     @Test(priority = 1)
     public void testRestAPI() throws InterruptedException {
@@ -47,6 +27,4 @@ public class RestAPI_Automation {
         Assert.assertEquals(GBPDescription, "British Pound Sterling", "GBP description not matching.");
     }
 
-    @AfterSuite
-    public void tearDown() {}
 }
